75f94525ea26eeeb0ca2810381bc61aea631a5ad
KerbsOpenSource <opensource@kerbs.dev>
2024-02-05 15:41:46 +0300

    🎉 first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# pwdgenz
+Command line password generator
+
+## 🤔 What is this?
+This is a utility that can generate passwords with a "smart" percentage of character selection. If the first character was a numeric character, when the second character is selected, the program reduces the chance of selecting a numeric character by 2 times, and the other characters increase their selection probability, and so on. 
+
+Standard chances of **!SELECTING A TYPE OF SYMBOL!**
+> + Numbers - 32%
+> + Lowercase letters - 32%
+> + Uppercase letters - 32%
+> + Special characters ('!', '@', '#', '$', '-') - 4%
+
+Standard password length - 16 characters
+
+## 👨🏻‍💻 How to use
+### Input - standart
+> pwdgenz
+> 
+Output - String of 16 characters
+> W7v9n1M6OhiY3X!0
+>
+
+### Input - selectable number of characters
+> pwdgenz 32
+>
+Output - String of 32 characters
+> 5bsZDq3qELO5x3PNk8l2@R$LM@hG-k7#
+
+### Input - selectable number of characters + selectable number of passwords
+> pwdgenz 32 3
+>
+Output - 3 Strings of 32 characters each
+> q82#JqR0-H3fd-Y7HwE41-6!DX@bfnUP  
+> dAH194z#nRh9BeQI7nuuK9!yAQ8-c@j@  
+> 2#wU2m-K!@Bn77R$D5R@!!W#05I2Uz-d  
+
+
+## 🔨 How to build
+```
+git clone https://github.com/KerbsOpenSource/pwdgenz
+cd pwdgenz
+cargo build -r
+```
+
+
+### License
++ MIT License ([LICENSE.md](./LICENSE.md) or http://opensource.org/licenses/MIT)
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use rand::distributions::{Distribution, Uniform};
+use rand::seq::SliceRandom;
+use rand::Rng;
+use rand_core::OsRng;
+use std::env;
+
+const LETTERSET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+const SPECSET: [char; 5] = ['!', '@', '#', '$', '-'];
+
+fn number_char() -> String {
+    let mut rng = OsRng;
+    let dist = Uniform::from(0..=9);
+    let random_num = dist.sample(&mut rng);
+    format!("{}", random_num)
+}
+
+fn letter_char(uppercase: Option<bool>) -> String {
+    let mut rng = rand::thread_rng();
+    let letter = LETTERSET.choose(&mut rng).unwrap();
+    if let Some(true) = uppercase {
+        format!("{}", letter.to_uppercase())
+    } else {
+        format!("{}", letter)
+    }
+}
+
+fn spec_char() -> String {
+    let mut rng = rand::thread_rng();
+    let letter = SPECSET.choose(&mut rng).unwrap();
+    format!("{}", letter)
+}
+
+fn char_type_choice(probs: &[f32]) -> Result<usize, &'static str> {
+    let mut rng = OsRng;
+    let total_prob = probs.iter().sum::<f32>();
+    let mut random_num: f32 = rng.gen_range(0.0..total_prob);
+    for (i, &prob) in probs.iter().enumerate() {
+        if random_num < prob {
+            return Ok(i);
+        }
+        random_num -= prob;
+    }
+    Err("Invalid probability distribution")
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut char_count: u32 = 16;
+    let mut password_count: u32 = 1;
+    if args.len() > 1 {
+        match args[1].parse::<u32>() {
+            Ok(num) => char_count = num,
+            Err(e) => eprintln!("Error: {}", e),
+        }
+        if args.len() > 2 {
+            match args[2].parse::<u32>() {
+                Ok(num) => password_count = num,
+                Err(e) => eprintln!("Error: {}", e),
+            }
+        }
+    }
+    let mut probs = vec![0.32, 0.32, 0.32, 0.04];
+    for i in 0..password_count {
+        let mut password = String::from("");
+        for _ in 0..char_count {
+            let char_type = match char_type_choice(&probs) {
+                Ok(i) => i,
+                Err(e) => panic!("{}", e),
+            };
+
+            match char_type {
+                0 => password.push_str(&number_char()),
+                1 => password.push_str(&letter_char(Some(false))),
+                2 => password.push_str(&letter_char(Some(true))),
+                3 => password.push_str(&spec_char()),
+                4_usize.. => todo!(),
+            };
+
+            // Update probabilities
+            probs[char_type] /= 2.0;
+            for prob in probs.iter_mut() {
+                *prob *= 1.33;
+                if *prob > 1.0 {
+                    *prob = 1.0;
+                }
+            }
+        }
+        print!("{}", password);
+        if i != password_count - 1 {
+            println!();
+        }
+    }
+}
