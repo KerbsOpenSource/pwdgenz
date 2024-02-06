5a22be88322fc6368ccb3055f0642b942d1026df
KerbsOpenSource <opensource@kerbs.dev>
2024-02-06 15:53:10 +0300

    Add flags, new interaction

--- README.md
+++ README.md
@@ -13,26 +13,15 @@ Standard chances of **!SELECTING A TYPE OF SYMBOL!**
 Standard password length - 16 characters
 
 ## 👨🏻‍💻 How to use
-### Input - standart
-> pwdgenz
-> 
-Output - String of 16 characters
-> W7v9n1M6OhiY3X!0
->
+```
+Usage: pwdgenz [OPTIONS]
 
-### Input - selectable number of characters
-> pwdgenz 32
->
-Output - String of 32 characters
-> 5bsZDq3qELO5x3PNk8l2@R$LM@hG-k7#
-
-### Input - selectable number of characters + selectable number of passwords
-> pwdgenz 32 3
->
-Output - 3 Strings of 32 characters each
-> q82#JqR0-H3fd-Y7HwE41-6!DX@bfnUP  
-> dAH194z#nRh9BeQI7nuuK9!yAQ8-c@j@  
-> 2#wU2m-K!@Bn77R$D5R@!!W#05I2Uz-d  
+Options:
+  -a, --amount <AMOUNT>  Amount of passwords [default: 1]
+  -l, --length <LENGTH>  Password length [default: 16]
+  -h, --help             Print help
+  -V, --version          Print version
+```
 
 
 ## 🔨 How to build
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use rand::distributions::{Distribution, Uniform};
 use rand::seq::SliceRandom;
 use rand::Rng;
 use rand_core::OsRng;
-use std::env;
+use clap::Parser;
 
 const LETTERSET: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 const SPECSET: [char; 5] = ['!', '@', '#', '$', '-'];
+
 
 fn number_char() -> String {
     let mut rng = OsRng;
@@ -46,26 +47,27 @@ fn char_type_choice(probs: &[f32]) -> Result<usize, &'static str> {
     Err("Invalid probability distribution")
 }
 
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Password length
+    #[arg(short, long, default_value_t = 16)]
+    length: u32,
+    /// Amount of passwords
+    #[arg(short, long, default_value_t = 1)]
+    amount: u32,
+}
+
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut char_count: u32 = 16;
-    let mut password_count: u32 = 1;
-    if args.len() > 1 {
-        match args[1].parse::<u32>() {
-            Ok(num) => char_count = num,
-            Err(e) => eprintln!("Error: {}", e),
-        }
-        if args.len() > 2 {
-            match args[2].parse::<u32>() {
-                Ok(num) => password_count = num,
-                Err(e) => eprintln!("Error: {}", e),
-            }
-        }
-    }
+    let cli = Cli::parse();
+    let password_amount: u32 = cli.amount;
+    let char_length: u32 = cli.length;
     let mut probs = vec![0.32, 0.32, 0.32, 0.04];
-    for i in 0..password_count {
+    for i in 0..password_amount {
         let mut password = String::from("");
-        for _ in 0..char_count {
+        for _ in 0..char_length {
             let char_type = match char_type_choice(&probs) {
                 Ok(i) => i,
                 Err(e) => panic!("{}", e),
@@ -89,7 +91,7 @@ fn main() {
             }
         }
         print!("{}", password);
-        if i != password_count - 1 {
+        if i != password_amount - 1 {
             println!();
         }
     }
